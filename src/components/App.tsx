df96fc883d45a187b0e863ee29fb0842d6f50776
Javier Farre <javier.farre@springpod.co.uk>
2021-02-27 17:30:40 +0000

    fixed prettier configuration before project start

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,42 +1,42 @@
-import React, { Fragment, useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { LoadingBar } from 'react-redux-loading'
+import React, { Fragment } from 'react';
+import { LoadingBar } from 'react-redux-loading';
+import { Switch } from 'react-router-dom';
 import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
 
 const App: React.FC = () => {
-  const authUser = useSelector(state => state.authUser);
-  const loading = useSelector(state => state.authUser === null);
+    // const authUser = useSelector(state => state.authUser);
+    // const loading = useSelector(state => state.authUser === null);
 
-  const dispatch = useDispatch();
+    // const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(handleInitialData());
-  }, [authUser])
+    // useEffect(() => {
+    //     dispatch(handleInitialData());
+    // }, [authUser])
 
-  return (
-    <Switch>
-      <Fragment>
-        <LoadingBar />
-        <div className="container">
+    return (
+        <Switch>
+            <Fragment>
+                <LoadingBar />
+                <h1>Hello world</h1>
+                {/* <div className="container">
 
-          {loading
-            ? null
-            : <div>
-              <Route exact path="/">
+                    {loading
+                        ? null
+                        : <div>
+                            <Route exact path="/">
 
-              </Route>
-              <Route path="/tweet/:id">
+                            </Route>
+                            <Route path="/tweet/:id">
 
-              </Route>
-              <Route path="/new">
+                            </Route>
+                            <Route path="/new">
 
-              </Route>
-            </div>}
-        </div>
-      </Fragment>
-    </Switch>
-  );
-}
+                            </Route>
+                        </div>}
+                </div> */}
+            </Fragment>
+        </Switch>
+    );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
 import './index.css';
 
+// const store = createStore(reducer, middleware);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    // <Provider store={store}>
+    <Router>
+        <App />
+    </Router>,
+    // </Provider>,
+    document.getElementById('root'),
 );
